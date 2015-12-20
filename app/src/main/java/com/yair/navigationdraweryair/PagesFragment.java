a65aebff02a2d38a124a307acd9bfa69cfba2091
yairJCT <irigo1@gmail.com>
2015-12-20 22:09:39 +0200

    Initial commit

---
+++ app/src/main/java/com/yair/navigationdraweryair/PagesFragment.java
@@ -0,0 +1,22 @@
+package com.yair.navigationdraweryair;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PagesFragment extends Fragment {
+
+    public PagesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_pages, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/yair/navigationdraweryair/WhatsHotFragment.java
@@ -0,0 +1,23 @@
+package com.yair.navigationdraweryair;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WhatsHotFragment extends Fragment {
+
+    public WhatsHotFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_whats_hot, container, false);
+
+        return rootView;
+    }
+}
