12b6be4599037ed233882f63cbdddb7e8050e445
yairJCT <irigo1@gmail.com>
2015-12-31 12:48:25 +0200

    button in Home fragment

--- app/src/main/java/com/yair/navigationdraweryair/HomeFragment.java
+++ app/src/main/java/com/yair/navigationdraweryair/HomeFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeFragment extends Fragment {
 
@@ -16,6 +18,15 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button button = (Button)rootView.findViewById(R.id.inHomeButton);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //  Toast.makeText(MainActivity.this, "Sending", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "hhhhhh", Toast.LENGTH_SHORT).show();
+            }
+        } );
 
         return rootView;
     }
